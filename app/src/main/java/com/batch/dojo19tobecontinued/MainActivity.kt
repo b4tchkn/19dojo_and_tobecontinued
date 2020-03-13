f8ee19b6103c17244c62ed764cb06d37b5020c68
batch <baaaakkrad@gmail.com>
2020-03-14 01:39:35 +0900

    remove no need code in MainActivity

--- app/src/main/java/com/batch/dojo19tobecontinued/MainActivity.kt
+++ app/src/main/java/com/batch/dojo19tobecontinued/MainActivity.kt
@@ -1,15 +1,8 @@
 package com.batch.dojo19tobecontinued
 
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.room.Room
-import com.batch.dojo19tobecontinued.friendlist.model.MyDatabase
-import com.batch.dojo19tobecontinued.friendlist.model.User
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,30 +12,5 @@ class MainActivity : AppCompatActivity() {
 
         pager.adapter = TabAdapter(supportFragmentManager, this)
         tab_layout.setViewPager(pager)
-
-        val data: Uri? = intent?.data
-        Log.d("DATA", data.toString())
-
-        val db = Room.databaseBuilder(applicationContext, MyDatabase::class.java, "user")
-            .build()
-
-        if (data != null) {
-            val uri = Uri.parse(data.toString())
-            val iam = uri.getQueryParameter("iam")
-            val tw = uri.getQueryParameter("tw")
-            val gh = uri.getQueryParameter("gh")
-
-            val user = User()
-            user.fullName = iam.toString()
-            user.twitter = tw.toString()
-            user.gitHub = gh.toString()
-
-            Toast.makeText(this, "Add user ${iam}", Toast.LENGTH_LONG).show()
-
-            thread {
-                db.userDao().insert(user)
-            }
-        }
-
     }
 }
